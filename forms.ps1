f38c9d0c98e44903fb7e69dcf6ba0b99243e1934
Esteban Artavia <Esteban_Artavia@mckinsey.com>
2018-02-11 19:33:13 -0600

    Added files

---
+++ forms.ps1
@@ -0,0 +1,66 @@
+﻿#Generated Form Function 
+function GenerateForm { 
+
+#region Import the Assemblies 
+[reflection.assembly]::loadwithpartialname("System.Windows.Forms") | Out-Null 
+[reflection.assembly]::loadwithpartialname("System.Drawing") | Out-Null 
+#endregion
+
+#region Generated Form Objects 
+$HelpDeskForm = New-Object System.Windows.Forms.Form 
+$UnlockAccountButton = New-Object System.Windows.Forms.Button 
+$InitialFormWindowState = New-Object System.Windows.Forms.FormWindowState 
+
+$handler_UnlockAccountButton_Click= 
+{ 
+#TODO: Place custom script here
+
+}
+
+$OnLoadForm_StateCorrection= 
+{#Correct the initial state of the form to prevent the .Net maximized form issue 
+$HelpDeskForm.WindowState = $InitialFormWindowState 
+}
+
+#---------------------------------------------- 
+#region Generated Form Code 
+$HelpDeskForm.Text = "Our Help Desk" 
+$HelpDeskForm.Name = "HelpDeskForm" 
+$HelpDeskForm.DataBindings.DefaultDataSourceUpdateMode = 0 
+$System_Drawing_Size = New-Object System.Drawing.Size 
+$System_Drawing_Size.Width = 265 
+$System_Drawing_Size.Height = 55 
+$HelpDeskForm.ClientSize = $System_Drawing_Size
+
+$UnlockAccountButton.TabIndex = 0 
+$UnlockAccountButton.Name = "UnlockAccountButton" 
+$System_Drawing_Size = New-Object System.Drawing.Size 
+$System_Drawing_Size.Width = 240 
+$System_Drawing_Size.Height = 23 
+$UnlockAccountButton.Size = $System_Drawing_Size 
+$UnlockAccountButton.UseVisualStyleBackColor = $True
+
+$UnlockAccountButton.Text = "UNLOCK Account"
+
+$System_Drawing_Point = New-Object System.Drawing.Point 
+$System_Drawing_Point.X = 13 
+$System_Drawing_Point.Y = 13 
+$UnlockAccountButton.Location = $System_Drawing_Point 
+$UnlockAccountButton.DataBindings.DefaultDataSourceUpdateMode = 0 
+$UnlockAccountButton.add_Click($handler_UnlockAccountButton_Click)
+
+$HelpDeskForm.Controls.Add($UnlockAccountButton)
+
+#endregion Generated Form Code
+
+#Save the initial state of the form 
+$InitialFormWindowState = $HelpDeskForm.WindowState 
+#Init the OnLoad event to correct the initial state of the form 
+$HelpDeskForm.add_Load($OnLoadForm_StateCorrection) 
+#Show the Form 
+$HelpDeskForm.ShowDialog()| Out-Null
+
+} #End Function
+
+#Call the Function 
+GenerateForm 
