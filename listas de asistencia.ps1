d434f1f67f0eb668c2d4732bea021a56c777c4c4
Esteban Artavia <Esteban_Artavia@mckinsey.com>
2018-05-31 12:36:48 -0600

    Completed changes requested by Capitana

--- listas de asistencia.ps1
+++ listas de asistencia.ps1
@@ -19,12 +19,14 @@ Write-Host 'Seleccione archivo que contiene los profes'
 $profeslistfilename = Get-FileName
 $profeslist = Import-Csv $profeslistfilename
 $emailtemplate = gc .\emailtemplate.html -Encoding UTF8
-$email = "liftinghandstest2@gmail.com" 
-$pass = "MyPass@123" 
-$SMTPServer = "smtp.gmail.com"
-$SMTPPort = "587"
-$gmailcred = Get-Credential "liftinghandstest2@gmail.com"
-
+do {
+    try {
+        
+        $emailcheck = Read-host "Desea Enviar correos? (Y/N)"
+        } # end try
+    catch {Write-Host 'escoja entre Y o N, no se ponga creativa'}
+    } # end do 
+until ($emailcheck -eq 'y' -or $emailcheck -eq 'n' )
 
 
 
@@ -33,16 +35,17 @@ $gmailcred = Get-Credential "liftinghandstest2@gmail.com"
 foreach ($profe in $profeslist) {
 $profecourseid = $profe.courseID
 
-$outcsv = $profecourseid+".csv"
-$outhtml = $profecourseid+".html"
+
+
 $outpng = $profecourseid+".png"
-$snapurl = "https://s3-us-west-2.amazonaws.com/liftinglistsnaps/"+$outpng
+#$snapurl = "https://s3-us-west-2.amazonaws.com/liftinglistsnaps/"+$outpng
 $profeid= $profe.profeid
 $curso = $profe.profecurso
 $profefull = $profe.profenombre+" "+$profe.profeapellido
-$profeemail = $profe.profeemail
+$profeemail = $profe.profeemai
 $profephone = $profe.profephone
-
+$outcsv = $profefull+"-"+$curso+"-"+$profecourseid+".csv"
+$outhtml = $profefull+"-"+$curso+"-"+$profecourseid+".html"
 ####definir dias#####
 
 $horas = $profe.short_name
@@ -67,12 +70,25 @@ $adddias = $addcourse -replace '@dias', $cursodia
 $addhora = $adddias -replace '@hora', $profe.profehorariostart
 $addhora | Out-File $outhtml
 $emailbody = gc $outhtml -Encoding UTF8 | Out-String 
-$Subject = "Email Subject"
-Write-Host $profeemail $snapurl
-$msgid = Get-Random -Minimum 3 -Maximum 9999
-python convert-csv-to-jpg.py $outcsv
-$whatsappapicall = 'https://www.waboxapp.com/api/send/media?token=2f9946f8f7dbfc1cfb8ddeb854b10f3a5aca527b4b2a7&uid=50689910903&to='+$profephone+'&custom_uid=msg'+$msgid+'&caption=test+alcahueta&url='+$snapurl
-Invoke-WebRequest $whatsappapicall
-Send-MailMessage -From $email -to $profeemail -Subject $Subject -Credential $gmailcred -Attachments $outcsv -Body $emailbody  -SmtpServer $SMTPServer  -port $SMTPPort -UseSsl -BodyAsHtml -Encoding UTF8
+$Subject = "Listas de Clase - Lifting Hands"
+#Write-Host $profeemail $snapurl
+#$msgid = Get-Random -Minimum 3 -Maximum 9999
+#python convert-csv-to-jpg.py $outcsv
+#$whatsappapicall = 'https://www.waboxapp.com/api/send/media?token=2f9946f8f7dbfc1cfb8ddeb854b10f3a5aca527b4b2a7&uid=50689910903&to='+$profephone+'&custom_uid=msg'+$msgid+'&caption=test+alcahueta&url='+$snapurl
+#Invoke-WebRequest $whatsappapicall
+
+if ($emailcheck -eq 'Y') {
+
+$email = "monica@liftinghands.org" 
+$pass = "####" 
+$SMTPServer = "smtp.gmail.com"
+$SMTPPort = "587"
+$gmailcred = Get-Credential "monica@liftinghands.org"
+
+
+Send-MailMessage -From $email -to $profeemail -Subject $Subject -Credential $gmailcred -Attachments $outcsv -Body $emailbody  -SmtpServer $SMTPServer  -port $SMTPPort -UseSsl -BodyAsHtml -Encoding UTF8 -Bcc 'esteban128a@hotmail.com', 'monica@liftinghands.org'
+$profeemail
+}
+
 } 
 
